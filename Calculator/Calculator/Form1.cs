a98be1a9fccbcdff2bef4ab05e6d47848af5e6a2
Aaminamm <amynmanoochehri9@gmail.com>
2024-03-12 02:12:29 +0330

    Main

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int num1;
+        int num2;
+        int result;
+        string str;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "";
+            str = "*";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "";
+            str = "+";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "";
+            str = "-";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Text = "";
+            str = "/";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            num2 = int.Parse(textBox1.Text);
+
+            switch(str)
+            {
+                case "+":result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+            }
+            textBox1.Text = result.ToString();
+        }
+    }
+}
